a82f418154386e2da31ea2bbb7915bd10a212366
obsessedyouth <52631736+obsessedyouth@users.noreply.github.com>
2020-10-12 02:56:41 +0100

    Revert "chore: heroku deployment config"
    This reverts commit 403cba1ac3532c142f8b27c0e9ef480f58cb266d.

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :obsessed_thoughts_server, ObsessedThoughtsServerWeb.Endpoint,
-       http: [port: {:system, "PORT"}],
-       url: [scheme: "https", host: "obsessedthoughts.herokuapp.com", port: 443],
-       force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
